336f6275cc50ffec2a3d9032f8c887cf225e7079
Abas Farah <abasnfarah@gmail.com>
2021-10-14 07:32:09 -0500

    Adds codeforce competition problems

---
+++ competitions/codeforces/A_Gamer_Hermose.cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<pll> vpll;
+typedef unordered_map<ll,ll> umap;
+typedef set<ll> uset;
+template<typename T, typename S> ostream &operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
+template <typename T> ostream &operator << (ostream &os, const vector<T> &v){os << "[";for (ll i = 0; i < v.size(); ++i) {os << v[i];if (i != v.size() - 1)os << ", ";}os << "]\n";return os;}
+template <typename T> ostream &operator << (ostream &os, const set<T> &v){os << "{";for (auto it : v) {os << it;if (it != *v.rbegin())os << ", ";}os << "}\n";return os;}
+template <typename T> ostream &operator << (ostream &os, const unordered_set<T> &v){os << "{";for (auto it : v) {os << it;if (it != *v.rbegin())os << ", ";}os << "}\n";return os;}
+template <typename T, typename S> ostream &operator<<(ostream &os, const map<T, S> &v) { os << "{ \n"; for (auto it : v) os << it.first << " : " << it.second << "\n"; os << " }\n"; return os;}
+template <typename T, typename S> ostream &operator<<(ostream &os, const unordered_map<T, S> &v) { os << "{ \n"; for (auto it : v) os << it.first << " : " << it.second << "\n"; os << " }\n"; return os;}
+#define loop(i, a, b) for(ll i=a;i<b;i++)
+#define For(i, n) for(int i=0;i<(ll)n;i++)
+#define Rev(i, n) for(int i=n;i>=0;i--)
+#define Rep(i, n) for(int i=1;i<=n;++i)
+#define all(v) (v).begin(),(v).end()
+#define mems(x, y) memset(x, y, sizeof(x))
+#define sz(v) (v).size()
+#define F first
+#define S second
+#define pb push_back
+#define mp(a,b) make_pair(a,b)
+#define pf(n) cout<<n<<"\n"
+#define pff(n) cout<<n<<" "
+#define gl(str, buff) fgets(str, buff, stdin)
+#define ar array
+#define mei(v) std::max_element((v).begin(),(v).end()) - (v).begin() 
+#define me(v) *std::max_element((v).begin(),(v).end())
+#define nei(v) std::min_element((v).begin(),(v).end()) - (v).begin() 
+#define ne(v) *std::min_element((v).begin(),(v).end())
+#define sum(v) accumulate(all(v), 0)
+#define case(i) cout<<"Case #"<<i<<": "
+#define tolower(s) transform(s.begin(),s.end(),s.begin(),[](unsigned char c){ return std::tolower(c); })
+#define toupper(s) transform(s.begin(),s.end(),s.begin(),[](unsigned char c){ return std::tolower(c); })
+ll max(ll a, ll b){ return a > b ? a : b; }
+ll min(ll a, ll b){ return a < b ? a : b; }
+#define debug(args...) { string _s = #args; replace(_s.begin(), _s.end(), ',', ' '); stringstream _ss(_s); istream_iterator<string> _it(_ss); err(_it, args); }
+
+void err(istream_iterator<string> it) {}
+template<typename T, typename... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+	cerr << *it << " = " << a << endl;
+	err(++it, args...);
+}
+//const ll inf = 1e16;
+const ll inf = numeric_limits<ll>::max();
+
+void solve(ll h, vll v){
+    // solution comes here
+    debug(h,v);
+
+    //ll idx = -1;
+    //ll c = 0;
+
+    //sort(all(v));
+    //while(h > 0){
+        //Rev(i, v.size() - 1){
+            //if(i != idx){
+                //debug(i)
+                //h-=v[i];
+                //idx = i;
+                //c++;
+                //break;
+            //}
+        //}
+    //}     
+    //debug(, c, h);
+    ll m1 = 0;
+    ll m2 = 0;
+    loop(i, 0, v.size()){
+        // looking for firstMax
+        if(v[i] >= m1){
+            m2 = m1;
+            m1 = v[i];
+        }else if(v[i] < m1){
+            if(m2 == -1 || m2 < v[i]){
+                m2 = v[i];
+            }
+        }
+    }
+    
+
+    //bool idx = true;
+    ll c = 0;
+    if(h%(m1+m2) == 0){
+        c = h / (m1+m2);
+        debug(c, m1, m2);
+        c*=2;
+        debug(c);
+    }else{
+        c =(h / (m1+m2));
+        debug(c, m1, m2);
+        c*=2;
+        c++;
+        debug(c);
+    }
+    
+    pf(c);
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    ll N;
+
+    scanf("%lld", &N);
+
+    while(N > 0){
+        ll n, h;
+        vll v;
+        scanf("%lld %lld", &n, &h);
+        while(n > 0){
+            ll val;
+            scanf("%lld", &val);
+            v.pb(val);
+            --n;
+        }
+        solve(h, v);
+        --N;
+    }
+    return 0;
+}
+
